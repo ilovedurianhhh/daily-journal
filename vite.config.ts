1d1a8bb50b9467cbfb7b13c403e48d7d8714f98b
ilovedurianhhh <18020585118@163.com>
2026-06-12 16:40:17 +0800

    chore: v1.5, change to prompt mode for update banner

--- vite.config.ts
+++ vite.config.ts
@@ -15,7 +15,7 @@ export default defineConfig({
     react(),
     tailwindcss(),
     VitePWA({
-      registerType: 'autoUpdate',
+      registerType: 'prompt',
       manifest: {
         name: 'Daily Journal',
         short_name: 'Journal',
